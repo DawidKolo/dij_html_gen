0ce1161f8f09fa99de9cfc4bbc53904966b56a53
DawidKolo <dawid_kolodziej@o2.pl>
2023-07-26 21:34:09 +0200

    v2.1 translation

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ for z in path:
     doesExist = os.path.exists(z)
     if not doesExist:
         os.mkdir(z)
-DIJ = 5 # enter number of DIJ files you would like to process in EODeliver
-files = 5 # enter number of file per DIJ file you would like to process in EODeliver
+
+
+
+DIJ = 1 # enter number of DIJ files you would like to process in EODeliver
+files = 1 # enter number of file per DIJ file you would like to process in EODeliver (max 9998)
 
 
 
 for x in range(1,DIJ+1):
-    ID = f"25AKKX{x:04d}XXXXXXXXXXXXXXXXXX"  # Enter some custom string of characters. This will be part of unique docInstanceID number.
+    ID = f"25AKKX{x:04d}XXXXXXXXXXXXXXXXXX"  # Replace some X's with custom string of characters. This will be part of unique docInstanceID number.
     for i in range(1,files+1):
         filename = f".\\htmlfolder\\file{x}_000{i:04d}.html"
         print(filename)
@@ -30,7 +33,7 @@ for x in range(1,DIJ+1):
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <!-- '''+str(ID) + f"{i:04d}" ''' -->
-<title>11-na-print</title>
+<title>dummy html</title>
 <style type="text/css">a:link {color:#FFFFFF} a:visited {color:#FFFFFF} a:active {color:#FFFFFF}
 .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td {line-height: 100%}
 .ExternalClass {width: 100%;}
@@ -41,16 +44,16 @@ style="background-color:#FFFFFF; margin-top:0; margin-left:0; margin-bottom:0; m
 <table align="center" cellspacing="0" cellpadding="0" width="794" style="border-collapse:collapse;background-color:#FFFFFF;">
 <tr style="vertical-align:top;line-height:38px;font-size:38px"><td width="38" height="38"><span style="font-size:1px;line-height:1px;">&nbsp;</span></td><td width="718" height="38"><span style="font-size:1px;line-height:1px;">&nbsp;</span></td><td width="38" height="38"><span style="font-size:1px;line-height:1px;">&nbsp;</span></td></tr>
 <tr style="vertical-align:top;">
-<td width="38"><span style="font-size:1px;line-height:1px;">&nbsp;</span></td><td width="718"><div style="line-height:17px;"><span style="color:#000000;font-family:Arial;font-size:15px;">Gentile cliente&#44; </span></div>
+<td width="38"><span style="font-size:1px;line-height:1px;">&nbsp;</span></td><td width="718"><div style="line-height:17px;"><span style="color:#000000;font-family:Arial;font-size:15px;">Dear Customer&#44; </span></div>
 <div style="font-size:12px;line-height:12px;">&nbsp;</div>
-<div style="line-height:17px;text-align:justify;"><span style="color:#000000;font-family:Arial;font-size:15px;">desideriamo informarti che una comunicazione &#232; disponibile on line nella tua </span><span style="color:#000000;font-family:Arial;font-size:15px;text-decoration:underline;">Area Clienti Agos</span><span style="color:#000000;font-family:Arial;font-size:15px;">&#44; alla Sezione &#8220;Comunicazioni Periodiche&#8221; di ciascun prodotto. </span></div>
+<div style="line-height:17px;text-align:justify;"><span style="color:#000000;font-family:Arial;font-size:15px;">This very important email was sent by EODeliver </span><span style="color:#000000;font-family:Arial;font-size:15px;text-decoration:underline;">Customer's Corner</span><span style="color:#000000;font-family:Arial;font-size:15px;">&#44; to the Section &#8220;Periodic communications&#8221; of each product. </span></div>
 <div style="font-size:12px;line-height:12px;">&nbsp;</div>
-<div style="line-height:17px;text-align:justify;"><span style="color:#000000;font-family:Arial;font-size:15px;">Ti invitiamo a prenderne visione e ti ringraziamo per aver scelto i nostri servizi on line&#44; ricordandoti la possibilit&#224; di richiedere in qualsiasi momento la modifica della modalit&#224; di invio delle comunicazioni contattandoci al Servizio Clienti&#47;entrando nella Sezione &#8220;Gestione delle Comunicazioni&#8221;. </span></div>
+<div style="line-height:17px;text-align:justify;"><span style="color:#000000;font-family:Arial;font-size:15px;">We invite you to read it and we thank you for choosing our online services&#44;  reminding you of the possibility of requesting a change in the method of sending communications at any time by contacting us at Customer Service&#47;by entering the &#8220;Communications Management Section&#8221;. </span></div>
 <div style="font-size:12px;line-height:12px;">&nbsp;</div>
-<div style="line-height:17px;"><span style="color:#000000;font-family:Arial;font-size:15px;">Servizio Clienti </span></div>
-<div style="line-height:17px;"><span style="color:#000000;font-family:Arial;font-size:15px;">Agos Ducato S.p.A.</span></div>
+<div style="line-height:17px;"><span style="color:#000000;font-family:Arial;font-size:15px;">Customer Service </span></div>
+<div style="line-height:17px;"><span style="color:#000000;font-family:Arial;font-size:15px;">Precisely Software</span></div>
 <div style="font-size:12px;line-height:12px;">&nbsp;</div>
-<div style="line-height:17px;text-align:justify;"><span style="color:#000000;font-family:Arial;font-size:11px;">---- 1 ---- mail viene inviata automaticamente e le risposte non potranno essere lette. Per ogni richiesta di informazioni non utilizzare il tasto &#34;Rispondi&#34;&#44; ma contattaci attraverso il form dedicato disponibile alla sezione Contatti del sito di Agos Ducato.</span></div>
+<div style="line-height:17px;text-align:justify;"><span style="color:#000000;font-family:Arial;font-size:11px;">This e-mail is sent automatically and replies cannot be read. For each information request, do not use the &#34;reply&#34;&#44; but contact us through the dedicated form available in the Contact section of the Precisely website.</span></div>
 <div style="font-size:12px;line-height:12px;">&nbsp;</div>
 <div style="line-height:17px;"><span style="color:#000000;font-family:Arial;font-size:15px;">&nbsp;</span></div>
 </td>
@@ -78,14 +81,14 @@ style="background-color:#FFFFFF; margin-top:0; margin-left:0; margin-bottom:0; m
 </jobdata>''')
         for i in range(1,files+1):
             file.write('''<document docID="''' +str(i)+ f'''" docInstanceID="'''+ str(ID) + f"{i:04d}" + '''">
-<DDSDocValue name="Email" type="text" len="22">user1@test.com</DDSDocValue>
-<DDSDocValue name="From" type="text" len="22">NOREPLY@AGOSDUCATO.NET</DDSDocValue>
-<DDSDocValue name="Subject" type="text" len="80">Comunicazione importante relativa al rapporto di finanziamento Agos n. 068272764</DDSDocValue>
-<AccNo>AMB;0037057626</AccNo>
-<StmtDate>2023-05-23</StmtDate>
+<DDSDocValue name="Email" type="text" len="14">user1@test.com</DDSDocValue>
+<DDSDocValue name="From" type="text" len="16">NOREPLY@TEST.NET</DDSDocValue>
+<DDSDocValue name="Subject" type="text" len="64">Important communication relating to the Precisely product report</DDSDocValue>
+<AccNo>PRE;0037057626</AccNo>
+<StmtDate>2023-07-25</StmtDate>
 <CustData>
 </CustData>
-<DDSDocValue name="ARCHIVE_INDEX" type="text" len="121">|dummy|68272764|0|20876632|NAME|SURNAME|2023-02-01|ATTXXX97B04F839E|AMBP|23/05/2023|03:00:28 |||user2@test.com|</DDSDocValue>
+<DDSDocValue name="ARCHIVE_INDEX" type="text" len="121">|dummy|68272764|0|20876632|NAME|SURNAME|2023-02-01|ACCXXX97B04F839E|AMBP|23/05/2023|03:00:28 |||user2@test.com|</DDSDocValue>
 </document>''')
         file.write('''</eGAD>''')
 
